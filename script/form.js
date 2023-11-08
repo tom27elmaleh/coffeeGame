a3a5b1512936fd18c8eba927d97cc94282f4f48f
Elmaleh Tom <tomelmaleh@hotmail.com>
2023-11-08 22:43:34 +0100

    v1

--- script/form.js
+++ script/form.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const playerForm = document.getElementById("player-form");
+  const videoPlayer = document.getElementById("video");
 
   playerForm.addEventListener("submit", function (event) {
     event.preventDefault();
@@ -7,11 +8,33 @@ document.addEventListener("DOMContentLoaded", function () {
     const firstName = document.getElementById("first-name").value;
     const lastName = document.getElementById("last-name").value;
     const email = document.getElementById("email").value;
-    const phone = document.getElementById("phone").value;
+    const number = document.getElementById("number").value;
 
-    if (firstName && lastName && email && phone) {
-      // Envoyer les données côté serveur (Node.js) pour les enregistrer dans la base de données.
-      // Vous pouvez ajouter du code de redirection ici si nécessaire.
+    if (firstName && lastName && email && number) {
+      fetch("http://localhost:3000/users/add", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          email: email,
+          firstName: firstName,
+          lastName: lastName,
+          number: number,
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          fetch("http://localhost:3000/wins/", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              nameLastWinner: "firstName",
+            }),
+          })
+            .then((response) => response.json())
+            .then((data) => {
+              console.log(data);
+            });
+        });
     }
   });
 });
--- script/index.js
+++ script/index.js
@@ -1,9 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const qrCode = document.getElementById("qr-code");
-  const video = document.getElementById("video");
+// index.js
 
-  qrCode.addEventListener("click", () => {
-    video.style.display = "block";
-    video.play();
-  });
+document.addEventListener("DOMContentLoaded", function () {
+  const video = document.getElementById("video");
+  function checkIsPlaying() {
+    // Effectuer une requête GET à la route /checkIsPlaying
+    fetch("http://localhost:3000/wins/checkIsPlaying")
+      .then((response) => response.json())
+      .then((data) => {
+        const isPlaying = data.isPlaying;
+        if (isPlaying === true) {
+          video.play();
+        }
+      })
+      .catch((error) => {
+        console.error("Une erreur s'est produite :", error);
+      });
+  }
+
+  // Appeler checkIsPlaying toutes les 1 seconde
+  setInterval(checkIsPlaying, 1000);
 });
